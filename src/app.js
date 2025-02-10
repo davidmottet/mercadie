9a3a76818150d2decc0ff5ee0f86d58de326f03a
davidmottet <contact@davidmottet.com>
2025-02-10 21:57:37 +0000

    Add OpenAI integration and static file serving for food data generator

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import config from '../config/default.js';
 import connectToDatabase from './database/connection.js';
-import { handle404, handleErrors } from './middlewares/errorHandler.js';
 import ingredientRoutes from './routes/ingredients.js';
 import measurementUnitRoutes from './routes/measurementUnit.js';
 import recipes from './routes/recipes.js'
 import auth from './routes/auth.js'
+import { handle404, handleErrors } from './middlewares/errorHandler.js';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
-app.use(handle404);
-app.use(handleErrors);
 
 connectToDatabase();
 
@@ -19,9 +21,13 @@ const apiBasePath = config.api.basePath;
 const apiVersion = config.api.version;
 const baseRoute = `${apiBasePath}/${apiVersion}`;
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(`${baseRoute}/ingredients`, ingredientRoutes);
 app.use(`${baseRoute}/measurement-units`, measurementUnitRoutes);
 app.use(`${baseRoute}/recipes`, recipes);
 app.use(`${baseRoute}/auth`, auth);
+
+app.use(handle404);
+app.use(handleErrors);
 
 export default app;
