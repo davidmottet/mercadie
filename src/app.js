a27305cb4867aa2a7928220ef28f6301f8089cae
davidmottet <contact@davidmottet.com>
2025-02-15 09:49:31 +0000

    Migrate to EJS templating and update project structure for dynamic rendering

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import recipes from './routes/recipes.js';
 import auth from './routes/auth.js';
 import generator from './routes/generator.js';
 import './services/cron.js';
+import { renderIngredientsPage } from './controllers/ingredients.js';
 import { handle404, handleErrors } from './middlewares/errorHandler.js';
 
 const app = express();
@@ -23,12 +24,20 @@ const apiBasePath = config.api.basePath;
 const apiVersion = config.api.version;
 const baseRoute = `${apiBasePath}/${apiVersion}`;
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, './views'));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(`${baseRoute}/ingredients`, ingredientRoutes);
 app.use(`${baseRoute}/measurement-units`, measurementUnitRoutes);
 app.use(`${baseRoute}/recipes`, recipes);
 app.use(`${baseRoute}/generator`, generator);
 app.use(`${baseRoute}/auth`, auth);
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.get('/ingredients', renderIngredientsPage);
 
 app.use(handle404);
 app.use(handleErrors);
