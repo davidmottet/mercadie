d7be222b538836e70095e39ad9f9b73770031689
davidmottet <contact@davidmottet.com>
2025-02-18 20:39:29 +0000

    Add sign-in and sign-up pages with dynamic form inputs and improved styling

--- src/routes/views.js
+++ src/routes/views.js
@@ -17,8 +17,25 @@ router.get('/', (req, res) => {
 router.get('/signin', (req, res) => {
     const user = req.user || {};
     const order = req.order || { length: 0 };
+    const inputs = [
+        { id: 1, type: "email", placeholder: "Email", value: `${user?.email ? user?.email : ''}`, name: 'email' },
+        { id: 2, type: "password", placeholder: "Password", value: `${user?.password ? user?.password : ''}`, name: 'password' },
+    ]
 
-    res.render('signin', { user, order });
+    res.render('signin', { user, order, inputs });
+});
+
+router.get('/signup', (req, res) => {
+    const user = req.user || {};
+    const order = req.order || { length: 0 };
+    const inputs = [
+        { id: 1, type: "text", placeholder: "Name", value: `${user?.name ? user?.name : ''}`, name: 'name' },
+        { id: 2, type: "email", placeholder: "Email", value: `${user?.email ? user?.email : ''}`, name: 'email' },
+        { id: 3, type: "text", placeholder: "Profile Picture Link", value: `${user?.image ? user?.image : ''}`, name: 'image' },
+        { id: 4, type: "password", placeholder: "Password", value: `${user?.password ? user?.password : ''}`, name: 'password' },
+    ]
+
+    res.render('signup', { user, order, inputs });
 });
 
 export default router;
